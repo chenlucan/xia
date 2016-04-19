95c1eb00ebf75a9fb330308070bc01dd406320af
lucan.chen <chenlucan@hotmail.com>
2016-04-19 21:26:46 +0800

    Pantry: able to SaveImg, md5 as key, index on Date/Type

--- filemanager.js
+++ filemanager.js
@@ -8,8 +8,7 @@ var FileManager = function (dir_list, on_img, on_movie) {
 	var on_movie_ = on_movie;
 	var hash_     = new hash.FileHash();
 
-	var discover = fd.FileDiscover(dir_list_, ['png','jpg','jpeg'], FoundFile);
-
+	var discover = new fd.FileDiscover(dir_list_, ['png','jpg','jpeg'], FoundFile);
 	function FoundFile(md) {
 		if (md.ext) {
 			if (md.ext === 'jpeg' || md.ext === 'jpg') {
@@ -21,7 +20,7 @@ var FileManager = function (dir_list, on_img, on_movie) {
 			} else if (md.ext === 'mov') {
 				console.log("mov Not supported yet: ", md);
 			} else if (md.ext === 'flv') {
-				console.log("flv supported yet: ", md);
+				console.log("flv Not supported yet: ", md);
 			} else {
 				console.log("Not supported data: ", md);
 			}
--- pantry.js
+++ pantry.js
@@ -1,10 +1,81 @@
 var LevelPouchDB = require('pouchdb');
-var db = new LevelPouchDB('mydb');
 
 var Pantry = function () {
+	var pantry_ = 'pantry_storage';
+	var db = new LevelPouchDB(pantry_);
+
+	this.SaveImg   = SaveImg;
+	this.GetAll    = GetAll;
+	this.GetByDate = GetByDate;
+	this.GetByType = GetByType;
+
+	SetUpIndex();
 
 	function SaveImg(img) {
-		
+		img['_id'] = img['birth_time'].toISOString()+'_'+img['type']+'_'+img['md5'];
+		db.put(img).then(function (response) {
+			// handle response
+		}).catch(function (err) {
+		});
+	}
+
+	function GetAll() {
+		db.allDocs({
+			include_docs: true,
+		  	attachments: true
+		}).then(function (result) {
+		  	// handle result
+		}).catch(function (err) {
+		  	console.log(err);
+		});
+	}
+
+	function GetByDate(date) {
+		db.query('birth_time_index', {
+			include_docs: true,
+		  	attachments: true,
+		  	startkey: date, 
+		  	endkey: date+'\uffff'
+		}).then(function (result) {
+		  	// handle result
+		}).catch(function (err) {
+		  	console.log(err);
+		});
+	}	
+
+	function GetByType(type) {
+		db.query('type_index', {
+			include_docs: true,
+		  	attachments: true,
+		  	startkey: type, 
+		  	endkey: type+'\uffff'
+		}).then(function (result) {
+		  	// handle result
+		}).catch(function (err) {
+		  	console.log(err);
+		});
+	}
+
+	function SetUpIndex() {
+		var bt_index = createIndex('birth_time_index', function (doc) {
+			emit(doc.birth_time);
+		});
+		var tp_index = createIndex('type_index', function (doc) {
+			emit(doc.type);
+		});
+		db.put(bt_index).then(function (doc) {}).catch(function (err) {});
+		db.put(tp_index).then(function (doc) {}).catch(function (err) {});
+		db.query(bt_index, {stale: 'update_after'});
+		db.query(tp_index, {stale: 'update_after'});
+	}
+
+	function createIndex(name, mapFunction) {
+		var doc = {
+			_id: '_design/' + name,
+			views: {}
+		};
+		doc.views[name] = { map: mapFunction.toString() };
+		return doc;
 	}
 }
 
