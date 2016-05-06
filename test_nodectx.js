004f964bcc8f498cffdfaa9ba610d6fe45febd6e
lucan <lucan.chen@zrdinvestment.com>
2016-05-06 10:54:26 +0800

    total 5 modules. todo 2 modules. It's my problem not having a clear plan. Nothing to do with others Don't be afraid of being alone. I know you need somebody to discuss with

---
+++ test_nodectx.js
@@ -0,0 +1,3 @@
+console.log('====================from node ctx:===', nw);
+console.log('====================from node ctx:===', document);
+console.log('====================from node ctx:===', window);
