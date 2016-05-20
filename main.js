530f175e6315d3addd054f7549acc9e234e9482a
lucan.chen <chenlucan@hotmail.com>
2016-05-20 22:30:57 +0800

    organize app code structure

--- main.js
+++ main.js
@@ -1,6 +1,23 @@
+var path   = require('path');
+var fs     = require('fs');
+
+var fm     = require('./filemanager.js');
+var pantry = require('./pantry.js');
+
+// we use pantry as our data home
+console.log('app home: ', path.dirname(nw.App.dataPath));
+var data_home   = path.join(path.dirname(nw.App.dataPath), 'pantry');
+var photos_home = path.join(data_home, 'photos');
+var db_home     = path.join(data_home, 'db');
+var records_by_date = {};
+
+InitializeApp();
+
+var db = new pantry.Pantry(db_home);
+db.GetAll(OnRecord);
+
 jQuery(document).ready(function($){
-	var timelineBlocks = $('.cd-timeline-block'),
-		offset = 0.8;
+	var timelineBlocks = $('.cd-timeline-block'), offset = 0.8;
 
 	//hide timeline blocks which are outside the viewport
 	hideBlocks(timelineBlocks, offset);
@@ -23,87 +40,36 @@ jQuery(document).ready(function($){
 			( $(this).offset().top <= $(window).scrollTop()+$(window).height()*offset && $(this).find('.cd-timeline-img').hasClass('is-hidden') ) && $(this).find('.cd-timeline-img, .cd-timeline-content').removeClass('is-hidden').addClass('bounce-in');
 		});
 	}
-
-	function createTimePoint(record) {
-		if (record['type'] = 'image') {
-			createImagePoint(record);
-		} else if (record['type'] = 'movie') {
-			createMoviePoint(record);
-		} else {
-			console.log('time record is not supported: ', record);
-		}
-	}
+});
 
 
 
-	// it use Default as data home
-	console.log('default data home: ', nw.App.dataPath);
-	var path = require('path');
-	// we use pantry as our data home
-	console.log('app home: ', path.dirname(nw.App.dataPath));
 
-	var data_home = path.join(path.dirname(nw.App.dataPath), 'pantry');
 
-	var photos_home = path.join(data_home, 'photos');
-	var db_home     = path.join(data_home, 'db');
 
-	console.log('our data home: ', data_home);
 
-	var fs = require('fs');
-	SetUpPantry();
 
-	function SetUpPantry() {
-		fs.mkdir(data_home, function(err) {
-			if (!err) {
-				fs.mkdir(photos_home, function(err) {});
-				fs.mkdir(db_home, function(err) {});
-			}
-		});
-	}
 
-	var fm = require('./filemanager.js');
-	var filemanager = new fm.FileManager(['/Users/lucan/Documents/backup-20160330'], OnFile, OnFile);
-	function OnFile(file) {
-		console.log('============OnFile');
-		var bdate = new Date(file['birth_time']);
+// function definitions
+function OnRecord(record) {
+	if (record['type'] === 'image') {
+		var bdate = new Date(record['birth_time']);
 		var y = bdate.getFullYear() + '';
 		var m = (bdate.getMonth() < 9 ? '0' : '')+(bdate.getMonth()+1);
 		var d = bdate.getDate();
 		var k = y + '' + m + '' + d;
 		if (!(k in records_by_date)) {
-			records_by_date[k] = [file];
+			records_by_date[k] = [record];
 			console.log('record key=========', k);
 		} else {
-			records_by_date[k].push(file);
+			records_by_date[k].push(record);
 			console.log('date length=========', k, '====', records_by_date[k].length);
 			if (records_by_date[k].length === 4) {
 				AddImageTimePoint(records_by_date[k]);
 			}
 		}
-
 	}
-
-	var pantry = require('./pantry.js');
-	var db = new pantry.Pantry(db_home);
-	var records_by_date = {};
-	db.GetAll(OnRecord);
-
-	function OnRecord(record) {
-		if (record['type'] === 'image') {
-			// var bdate = new Date(record['birth_time']);
-			// var y = bdate.getFullYear();
-			// var m = bdate.getMonth() + 1;
-			// var d = bdate.getDate();
-			// var k = y + '' + m + '' + d;
-			// if (!(k in records_by_date)) {
-			// 	records_by_date[k] = [];
-			// 	console.log('record key=========', k);
-			// } else {
-			// 	records_by_date[k].append(file);
-			// 	console.log('date length=========', k, '====', records_by_date[k].lenth);
-			// }
-		}
-	}
+}
 
 function AddImageTimePoint(files) {
 	var t = document.querySelector('#img-template');
@@ -137,6 +103,19 @@ function AddImageTimePoint(files) {
 	var clone = document.importNode(t.content, true);
 	var tl = document.body.querySelector('#cd-timeline');
 
-  tl.insertBefore(clone, tl.firstChild);
+	tl.insertBefore(clone, tl.firstChild);
 }
-});
+
+// only effective for app first time setup
+function InitializeApp() {
+	SetUpPantry();
+
+	function SetUpPantry() {
+		fs.mkdir(data_home, function(err) {
+			if (!err) {
+				fs.mkdir(photos_home, function(err) {});
+				fs.mkdir(db_home, function(err) {});
+			}
+		});
+	}
+}
