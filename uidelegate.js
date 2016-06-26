b5b04e685068f6e7107044fb9c888d279b3be6c5
lucan.chen <chenlucan@hotmail.com>
2016-06-26 22:24:01 +0800

    1.load all images 2.display none for >= 4 3.use id_path 4.don’t set img src to avoid not-sufficient-resources 5.insert img at the end

--- uidelegate.js
+++ uidelegate.js
@@ -29,26 +29,30 @@ var TLManager = function() {
 			var d = bdate.getDate();
 			var date_id = y + '年' + m + '月' + d + '日';
 
-      if (date_id in tl_nodes_) {
-        if (tl_nodes_[date_id]['img_count'] >= max_img_count_) {
-          return;
-        }
-      }
-
       // {id:date_id, img_count:max 4, img_node: html element}
       GetNode(date_id);
+
       var node = document.body.querySelector('#id-'+date_id);
       var node_content = node.querySelector('.cd-timeline-content');
 
       var img_ph = document.body.querySelector('#img-placeholder-template');
       var empty_div = img_ph.content.querySelector('.tl_responsive');
-      empty_div.querySelector('a').href  = 'file://'+record['path'];
+
+      if (tl_nodes_[date_id]['img_count'] >= max_img_count_) {
+        empty_div.style.display = "None";
+        empty_div.querySelector('img').src = '' ;
+      } else {
+        empty_div.style.display = "";
+        empty_div.querySelector('img').src = 'file:///'+record['id_path'];
+      }
+      empty_div.querySelector('a').href  = 'file:///'+record['id_path'];
       empty_div.querySelector('a').setAttribute('data-lightbox', date_id);
-      empty_div.querySelector('img').src = 'file://'+record['path'];
       empty_div.querySelector('img').alt = record['name'];
       empty_div.querySelector('.tl_desc').innerHTML   = ""
+
       var clone = document.importNode(empty_div, true);
-      node_content.insertBefore(clone, node_content.childNodes[1]);
+      var all_imgs = node_content.getElementsByClassName('tl_responsive');
+      node_content.insertBefore(clone, node_content.querySelector('h2'));
       tl_nodes_[date_id]['img_count'] += 1;
 		}
   }
@@ -71,7 +75,7 @@ var TLManager = function() {
                       'id':date_id,
                       'img_count': 0,
                       'node':clone
-                   };;
+                   };
     return tl_nodes_[date_id];
   }
 }
